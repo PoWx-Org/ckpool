2961c2abbc1ed07822fa8e6e71812bcb274f6a39
Vsevolod Hulchuk <sevka.kiev@gmail.com>
2021-04-29 01:14:11 +0300

    Add some sql functions and sql scripts

---
+++ tools/scripts/install.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE IF NOT EXISTS pool_base;
+USE pool_base;
+
+CREATE TABLE IF NOT EXISTS `mined_blocks` (
+  `id_block` int unsigned NOT NULL,
+  `hash` varchar(256) NOT NULL,
+  `date_mined` datetime NOT NULL,
+  `height` int unsigned NOT NULL,
+  `reward` decimal(14,9) unsigned NOT NULL,
+  PRIMARY KEY (`id_block`),
+  UNIQUE KEY `block_id_UNIQUE` (`id_block`),
+  UNIQUE KEY `hash_UNIQUE` (`hash`),
+  UNIQUE KEY `date_mined_UNIQUE` (`date_mined`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE IF NOT EXISTS `miners` (
+  `id_miner` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_miner`),
+  UNIQUE KEY `id_miner_UNIQUE` (`id_miner`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `blocks_stats` (
+  `id_stat` int unsigned NOT NULL AUTO_INCREMENT,
+  `id_block` int unsigned NOT NULL,
+  `id_miner` int unsigned NOT NULL,
+  `shares` int DEFAULT NULL,
+  PRIMARY KEY (`id_stat`),
+  KEY `id_miner_foreign_idx` (`id_miner`),
+  KEY `id_block_foreign_idx` (`id_block`),
+  CONSTRAINT `id_block_foreign` FOREIGN KEY (`id_block`) REFERENCES `mined_blocks` (`id_block`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `id_miner_foreign` FOREIGN KEY (`id_miner`) REFERENCES `miners` (`id_miner`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- tools/utils.py
+++ tools/utils.py
@@ -1,4 +1,5 @@
 
+import os
 
 def get_reward(block_info, reward_addr):
     def is_coinbase(txn):
@@ -24,3 +25,52 @@ def get_reward(block_info, reward_addr):
     except:
         raise Exception("Seems like wrong address mentioned")
     return reward
+
+
+import json
+import pymysql
+sql_conf_file_name="parser.conf"
+
+with open(sql_conf_file_name, "r") as sql_conf_file:
+    sql_conf_str = sql_conf_file.read()
+    sql_conf = json.loads(sql_conf_str)['sql']
+
+username = sql_conf['auth']
+password = sql_conf['pass']
+hostname = sql_conf['host']
+
+
+def read_users:
+
+        
+def execute_query(query):
+    myConnection = pymysql.connect( host=hostname, user=username, passwd=password)
+    print(query)
+    cur = myConnection.cursor()
+    execute_complex_query(cur, query)
+    myConnection.commit()
+    myConnection.close()
+
+def execute_complex_query(cur, general_query):
+    queries = [query.strip() for query in general_query.split(';') if len(query) > 0]
+    for subquery in queries:
+        cur.execute(subquery)
+
+def install_db():
+    install_query_path = os.path.join('scripts', 'install.sql')
+    with open(install_query_path, 'r') as query_file:
+        install_query = query_file.read()
+    execute_query(install_query)
+
+def add_mined_block(hash, date, height, reward):
+    query = f'''USE `pool_base`; INSERT INTO `mined_blocks` 
+            (`hash`, `date_mined`, `height`, `reward`) 
+            VALUES ('{hash}', '{date}', {height}, {reward});'''
+    execute_query(query)
+
+def insert_stats
+
+install_db()
+add_mined_block('qwer', '2015-11-05 14:29:36', 21, 0.12345)
+execute_query('SELECT * FROM pool_base.mined_blocks;')
+
